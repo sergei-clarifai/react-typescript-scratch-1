4f6985f316ec3bd155ce812262a6f1ef441570dd
sergei-clarifai <68540853+sergei-clarifai@users.noreply.github.com>
2022-01-04 00:45:02 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="wrapper">
+      <h1>
+        React 17 and TypeScript 4 App!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
+
+
